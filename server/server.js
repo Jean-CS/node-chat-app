1ba5c4ba1cab6c999945d4d25e57676758691280
Jean Santos <jeansantosc@gmail.com>
2018-08-03 22:24:38 -0300

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -21,13 +21,32 @@ io.on('connection', (socket) => {
   // listens for an event
   socket.on('createMessage', (message) => {
     console.log('Creating message', message);
-    
+
+    socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app',
+      createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'New user joined',
+      createdAt: new Date().getTime()
+    })
+
     // emits an event to ALL CONNECTIONS
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
-      createdAt: new Date()
+      createdAt: new Date().getTime()
     })
+
+    // // emits even to all connections EXCEPT THIS CONNECTION(socket)
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date()
+    // })
   })
 
   socket.on('disconnect', () => {
@@ -38,4 +57,4 @@ io.on('connection', (socket) => {
 server.listen(port, () => console.log('Server up on port', port))
 
 // call app.listen on port 3000 - server is up on port 3000
-// 
+//
